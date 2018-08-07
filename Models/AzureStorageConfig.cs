6c540e6db09624b7e8e1286ca015ef1ab09e36c8
Nick Walker <nickwalk@microsoft.com>
2018-08-06 22:38:17 -0700

    .

---
+++ Models/AzureStorageConfig.cs
@@ -0,0 +1,11 @@
+namespace blob_app.Models
+{
+    public class AzureStorageConfig
+    {
+        public string AccountName { get; set; }
+        public string AccountKey { get; set; }
+        public string QueueName { get; set; }
+        public string ImageContainer { get; set; }
+        public string ThumbnailContainer { get; set; }
+    }
+}
